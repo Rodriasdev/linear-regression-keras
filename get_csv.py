ebcb55644ec7155dbcf8b857494a0a67552ceefe
Rodriasd <veronrodrigo98@gmail.com>
2024-09-03 09:47:18 -0300

    was created the sequential model

--- get_csv.py
+++ get_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def getCsv(file):
     datos = pd.read_csv(file, sep=",", skiprows=32, usecols=[0,1])
-    x = datos.iloc[:,0]
-    y =  datos.iloc[:,1]
+    x = datos.iloc[:,0].values
+    y =  datos.iloc[:,1].values
 
     return x,y
--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from keras.api.optimizers import SGD
 from get_csv import getCsv
 
 
-x,y = getCsv("altura_peso.csv")
+x, y = getCsv("altura_peso.csv")
 
 
 modelo = Sequential()
-modelo.add(Dense(x,y,activation='linear'))
-sgd = SGD(lr=0.0004)
+modelo.add(Dense(1,input_dim=1, activation='linear')) 
+
+
+sgd = SGD(learning_rate=0.0004)
+
 
 modelo.compile(loss='mse', optimizer=sgd)
+
+
 modelo.summary()
